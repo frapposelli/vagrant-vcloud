5876b7daea50701da9a986949b267fd376ae5cc2
Marc Siegfriedt <marc@techaccelerator.com>
2014-11-16 09:57:42 -0800

    fixed typo, moved set_vm_hardware back to power_on

--- lib/vagrant-vcloud/action/build_vapp.rb
+++ lib/vagrant-vcloud/action/build_vapp.rb
@@ -291,29 +291,6 @@ module VagrantPlugins
           new_vm_properties = new_vapp[:vms_hash].fetch(vm_name)
           env[:machine].id = new_vm_properties[:id]
 
-          # add vm metadata
-          if !cfg.metadata_vm.nil?
-            env[:ui].info('Setting VM metadata...')
-            set_metadata_vm = cnx.set_vm_metadata env[:machine].id, cfg.metadata_vm
-            cnx.wait_task_completion(set_metadata_vm)
-          end
-
-          # add/update hardware
-          env[:ui].info('Setting VM hardware...')
-          set_vm_hardware = cnx.set_vm_hardware(env[:machine].id, cfg)
-          if set_vm_hardware
-            cnx.wait_task_completion(set_vm_hardware)
-          end
-
-          # enable nested hypervisor
-          if !cfg.nested_hypervisor.nil? && cfg.nested_hypervisor == true
-            env[:ui].info('Enabling nested hypervisor...')
-            set_vm_nested_hypervisor = cnx.set_vm_nested_hypervisor(env[:machine].id, true)
-            if set_vm_nested_hypervisor
-              cnx.wait_task_completion(set_vm_nested_hypervisor)
-            end
-          end
-
           @app.call env
         end
       end
--- lib/vagrant-vcloud/action/power_on.rb
+++ lib/vagrant-vcloud/action/power_on.rb
@@ -13,6 +13,29 @@ module VagrantPlugins
           cfg = env[:machine].provider_config
           cnx = cfg.vcloud_cnx.driver
 
+          # add vm metadata
+          if !cfg.metadata_vm.nil?
+            env[:ui].info('Setting VM metadata...')
+            set_metadata_vm = cnx.set_vm_metadata env[:machine].id, cfg.metadata_vm
+            cnx.wait_task_completion(set_metadata_vm)
+          end
+
+          # add/update hardware
+          env[:ui].info('Setting VM hardware...')
+          set_vm_hardware = cnx.set_vm_hardware(env[:machine].id, cfg)
+          if set_vm_hardware
+            cnx.wait_task_completion(set_vm_hardware)
+          end
+
+          # enable nested hypervisor
+          if !cfg.nested_hypervisor.nil? && cfg.nested_hypervisor == true
+            env[:ui].info('Enabling nested hypervisor...')
+            set_vm_nested_hypervisor = cnx.set_vm_nested_hypervisor(env[:machine].id, true)
+            if set_vm_nested_hypervisor
+              cnx.wait_task_completion(set_vm_nested_hypervisor)
+            end
+          end
+
           if cfg.power_on.nil? || cfg.power_on == true
             env[:ui].info('Powering on VM...')
             poweron_vm = cnx.poweron_vm(env[:machine].id)
