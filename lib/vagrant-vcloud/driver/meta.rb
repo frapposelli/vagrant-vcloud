c497d560b2ebb7c528f3824149fc1f231a77cac3
Stefan Scherer <scherer_stefan@icloud.com>
2018-05-28 15:18:12 +0200

    Use TLSv1_2 to support vCD 9.1

--- lib/vagrant-vcloud/driver/meta.rb
+++ lib/vagrant-vcloud/driver/meta.rb
@@ -105,8 +105,8 @@ module VagrantPlugins
           # Create a new HTTP client
           clnt = HTTPClient.new
 
-          # Set SSL proto to TLSv1
-          clnt.ssl_config.ssl_version = :TLSv1
+          # Set SSL proto to TLSv1_2
+          clnt.ssl_config.ssl_version = :TLSv1_2
 
           # Disable SSL cert verification
           clnt.ssl_config.verify_mode = (OpenSSL::SSL::VERIFY_NONE)
